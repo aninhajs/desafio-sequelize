956cbfb86845e4827b69c26827d21d5e5b572b5e
Ana Jeize <anajeize27@gmail.com>
2026-04-23 22:26:43 -0300

    feat: ultimas mudanças feitas em app.js, database.js, usuariocontrollers e usuariosroutes.js e também teve mudança no package.json

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const UsuarioController = require("../controllers/UsuarioControlller");
 
-// Rotas de Usuário
-router.post("/usuarios", UsuarioController.criarUsuario);
-router.get("/usuarios", UsuarioController.listarUsuarios);
-router.get("/usuarios/:id", UsuarioController.buscarUsuarioPorId);
+// Rotas de Usuário - CRUD Completo
+router.post("/", UsuarioController.criarUsuario);
+router.get("/", UsuarioController.listarUsuarios);
+router.get("/:id", UsuarioController.buscarUsuarioPorId);
+router.put("/:id", UsuarioController.atualizarUsuario);
+router.delete("/:id", UsuarioController.deletarUsuario);
 
 module.exports = router;
